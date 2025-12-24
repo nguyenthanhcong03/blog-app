868c6929f7f52da63dd962e53ced00ae45881e5e
Dinh Tran <23020043@vnu.edu.vn>
2025-12-24 09:05:17 +0700

    dinhtran: docker

--- be/BlogApp/BlogApp/Program.cs
+++ be/BlogApp/BlogApp/Program.cs
@@ -140,6 +140,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
